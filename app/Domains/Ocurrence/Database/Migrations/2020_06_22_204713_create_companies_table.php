cfe881257105017919b31bb4d762b4b9e214abc3
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-06-22 17:07:31 -0400

    added migrations for companies and incidents table

---
+++ app/Domains/Ocurrence/Database/Migrations/2020_06_22_204713_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('company_id')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->string('whatsapp');
+            $table->string('city');
+            $table->string('uf', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ app/Domains/Ocurrence/Database/Migrations/2020_06_22_204719_create_incidents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('company_id')->unsigned();
+            $table->uuid('incident_id');
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('value');
+            $table->foreign('company_id')->references('company_id')->on('companies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('incidents');
+    }
+}
