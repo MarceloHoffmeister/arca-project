bd3735cf3996414bc35bc2e6d19f700a32a765c0
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2020-06-22 18:20:15 -0400

    added creation method

--- app/Domains/Ocurrence/Http/Controllers/IncidentController.php
+++ app/Domains/Ocurrence/Http/Controllers/IncidentController.php
@@ -4,10 +4,24 @@
 namespace Arca\Domains\Ocurrence\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+
 class IncidentController
 {
-    public function index(): bool
+    public function index(): string
     {
-        return true;
+        return 'Hello World';
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            'title' => 'string|required',
+            'description' => 'string|required',
+            'value' => 'decimal|required'
+        ]);
+
+
+        return response()->json('success!');
     }
 }
